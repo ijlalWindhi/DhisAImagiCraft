7df93c44776e512491063ccf8aa466982793e1c1
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-02-27 16:24:36 +0700

    feat(sidebar):sign in sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import Navbar from "@/components/shared/Navbar";
+import Sidebar from "@/components/shared/Sidebar";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="root">
+      {/* Sidebar component */}
+      <Sidebar />
+
+      {/* Navbar component */}
+      <Navbar />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
---
+++ components/shared/Sidebar.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { navLinks } from "@/constants";
+import { SignedIn } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+
+  return (
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href={"/"} className="sidebar-logo">
+          <Image
+            src="/assets/images/logo-text.svg"
+            alt="Logo DhisAImagiCraft"
+            width={100}
+            height={28}
+          />
+        </Link>
+
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link href={link.route} className="sidebar-link">
+                      <Image
+                        src={link.icon}
+                        alt={`icon ${link.label}`}
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
