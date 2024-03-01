6f63a983feef649787e4f24fe469c8804be2d770
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-03-01 13:59:45 +0700

    feat(layout):components mobile navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,14 @@
-import Navbar from "@/components/shared/Navbar";
 import Sidebar from "@/components/shared/Sidebar";
+import MobileNav from "@/components/shared/MobileNav";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="root">
+      {/* MobileNav component */}
+      <MobileNav />
+
       {/* Sidebar component */}
       <Sidebar />
-
-      {/* Navbar component */}
-      <Navbar />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
